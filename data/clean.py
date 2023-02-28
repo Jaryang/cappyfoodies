c081f95ad8a7d146923713edeb899583d4714834
Miao Li <mli628@linux6.cs.uchicago.edu>
2023-02-28 16:03:56 -0600

    finish labelling restaurants

--- data/clean.py
+++ data/clean.py
@@ -2,6 +2,27 @@ import pandas as pd
 import re
 import csv
 
+cate_dct = {
+    'Bakeries' : ["Bakeries", "Piadina", "Pretzels", "Bagels", 'Patisserie/Cake Shop'],
+    'Alcohol' : ['Cocktail Bars','Brewpubs','Beer Bar','Whiskey Bars','Wine Bars','Beer, Wine & Spirits', 'Pubs', 'Bars','Dive Bars', \
+                 'Sports Bars','Beer', 'Wine & Spirits', 'Breweries', 'Cideries', 'Distilleries', 'Meaderies', 'Wineries', 'Wine Tasting Room'],
+    'Coffee_Tea' : ['Coffee & Tea','Coffee Roasteries', 'Tea Rooms,Bubble Tea', 'Cafes'],
+    'Dessert' : ['Creperies','Candy Stores','Pancakes','Desserts','Chimney Cakes','Cupcakes','Custom Cakes','Donuts', 'Gelato', 'Honey', \
+                 'Ice Cream & Frozen Yogurt', 'Milkshake Bars', 'Shaved Ice', 'Shaved Snow', 'Candy Stores', 'Chocolatiers & Shops', 'Macarons'],
+    'Grocery' : ['Imported Food', 'International Grocery','Organic Stores','Fruits & Veggies','Health Markets','Convenience Stores','Farmers Market','CSA'],
+    'Beverage' : ['Juice Bars & Smoothies', 'Acai Bowls','Kombucha', 'Water Stores', 'Bubble Tea'],
+    'Fast_Food': ['Cheesesteaks','Burgers', 'Chicken Wings','Fast Food','Pizza', 'Hot Dogs', 'Sandwiches'],
+    'Specialty_Store' : ['Cheese Shops','Herbs & Spices','Olive Oil','Pasta Shops','Popcorn Shops','Tofu Shops','Empanadas'],
+    'Meat_Shop' : ['Seafood Markets', 'Butcher', 'Smokehouse'],
+    'Regional' : ['Cuban', 'Venezuelan','Honduran','Vietnamese','Afghan','African','Bulgarian','Colombian', 'Japanese', 'Laotian', 'Latin American', \
+                  'Mediterranean', 'Mexican', 'Middle Eastern', 'Modern European', 'New Mexican Cuisine', 'Pakistani','Pan Asian', 'Polish', 'Puerto Rican', 'Scottish', \
+                  'Southern','Thai', 'Turkish','Chinese', 'Czech','Filipino','French','Greek', 'Hawaiian', 'Indian', 'Irish', 'Korean','German','Italian',\
+                  'Japanese','Czech','American (Traditional)','American (New)','Asian Fusion', 'Cajun/Creole','Cantonese','Caribbean']
+    }
+Not_Food = ['Kids Activities', 'Music Venues', 'Arcades','Venues & Event Spaces','Party & Event Planning','Comedy Clubs', \
+            'Street Vendors','Bookstores','Lounges', 'Vape Shops','Gift Shops']
+Sub_Category = ['Halal','Noodles','Tacos', 'Sushi Bars','Soul Food', 'Dim Sum', 'Buffets', 'Cafeteria', 'Food Trucks','Gluten-Free','Caterers','Vegan','Diners']
+
 def clean_edu(filename):
     '''
     take in the education data downloaded from census bureau and clean it
@@ -43,8 +64,8 @@ def clean_income(filename):
     df.rename(columns = lambda x: x.replace('ZCTA5 ',"")[:5], inplace = True)
     df = df.T
     df['NAME'] =df.index
-    df['med_hd_inc'] = df. iloc[:,0]
-    df['mean_hd_inc'] = df. iloc[:,1]
+    df['med_hd_inc'] = df.iloc[:,0]
+    df['mean_hd_inc'] = df.iloc[:,1]
     df.drop(df.iloc[:, 0:2], inplace=True, axis=1)
 
     return df
@@ -67,37 +88,39 @@ def clean_pop(filename):
     return data
 
 def relabel(cat_lst):
-    cate_dct = {
-    'Bakeries' : ["Bakeries", "Piadina", "Pretzels", "Bagels", 'Patisserie/Cake Shop'],
-    'Alcohol' : ['Cocktail Bars','Brewpubs','Beer Bar','Whiskey Bars','Wine Bars','Beer, Wine & Spirits', 'Pubs', 'Bars','Dive Bars', \
-                 'Sports Bars','Beer', 'Wine & Spirits', 'Breweries', 'Cideries', 'Distilleries', 'Meaderies', 'Wineries', 'Wine Tasting Room'],
-    'Coffee_Tea' : ['Coffee & Tea','Coffee Roasteries', 'Tea Rooms,Bubble Tea', 'Cafes'],
-    'Dessert' : ['Candy Stores','Pancakes','Desserts','Chimney Cakes','Cupcakes','Custom Cakes','Donuts', 'Gelato', 'Honey', \
-                 'Ice Cream & Frozen Yogurt', 'Milkshake Bars', 'Shaved Ice', 'Shaved Snow', 'Candy Stores', 'Chocolatiers & Shops', 'Macarons'],
-    'Grocery' : ['Imported Food', 'International Grocery','Organic Stores','Fruits & Veggies','Health Markets','Convenience Stores','Farmers Market','CSA'],
-    'Beverage' : ['Juice Bars & Smoothies', 'Acai Bowls','Kombucha', 'Water Stores', 'Bubble Tea'],
-    'Fast_Food': ['Burgers', 'Chicken Wings','Fast Food','Pizza', 'Hot Dogs', 'Sandwiches'],
-    'Specialty_Store' : ['Cheese Shops','Herbs & Spices','Olive Oil','Pasta Shops','Popcorn Shops','Tofu Shops','Empanadas'],
-    'Meat_Shop' : ['Seafood Markets', 'Butcher', 'Smokehouse'],
-    'Regional' : ['Cuban','Venezuelan','Honduran','Vietnamese','Afghan','African','Bulgarian','Colombian', 'Japanese', 'Laotian', 'Latin American', \
-                  'Mediterranean', 'Mexican', 'Middle Eastern', 'Modern European', 'New Mexican Cuisine', 'Pakistani','Pan Asian', 'Polish', 'Puerto Rican', 'Scottish', \
-                  'Southern','Thai', 'Turkish','Chinese', 'Czech','Filipino','French','Greek', 'Hawaiian', 'Indian', 'Irish', 'Korean','German','Italian',\
-                  'Japanese','Czech','American (Traditional)','American (New)','Asian Fusion', 'Cajun/Creole','Cantonese','Caribbean']
-    }
-    Not_Food = ['Kids Activities','Music Venues','Arcades','Venues & Event Spaces','Party & Event Planning','Comedy Clubs','Bookstores','Lounges', 'Vape Shops','Gift Shops']
+    
+    new_label = []
     for label in cat_lst:
-        if label in Not_Food:
-            cat_lst.remove(label)
+        if label not in Not_Food and label not in Sub_Category:
+            new_label.append(label)
 
-    for i, label in enumerate(cat_lst):
+    for i, label in enumerate(new_label):
         for category, lst in cate_dct.items():
             if label in lst:
-                cat_lst[i] = category
+                new_label[i] = category
         
-    return list(set(cat_lst))
+    return list(set(new_label))
+
+def find_cat(cat_lst, new_cat):
+    '''
+    if category in new_cat, return the most common one
+    if not: return 'other'
+    '''
+    find_label = False
+
+    for category in cat_lst:
+        for label in new_cat:
+            if category == label:
+                find_label = True
+                return label
+            
+    if not find_label:
+        return "other" 
 
 def clean_rest(filename):
-
+    '''
+    Taking in the yelp data and relabel restaurants for data viz
+    '''
     with open(filename) as f:
         reader = csv.DictReader(f)
         data = []
@@ -115,16 +138,32 @@ def clean_rest(filename):
         for type in relabel_lst:
             counter[zipcode][type] = counter[zipcode].get(type, 0) + 1
 
-
-    top_5_food = {}
+    top_3_food = {}
     sum_lst = []
 
     for key, value in counter.items():
-        #change to modify top ones to have
         lst = sorted(value, key=lambda i: value[i])[-3:]
         sum_lst.extend(lst)
-        top_5_food[key] = lst
+        top_3_food[key] = lst
     
-    return top_5_food
+    for restaurant in data:
+        cat_lst = eval(restaurant['categories'])
+        zipcode = restaurant['zip_code']
+        new_cat = top_3_food[zipcode]
+        food_label = find_cat(cat_lst, new_cat)
+        regional_label = find_cat(cat_lst, cate_dct['Regional'])
+        restaurant['food_label'] = food_label
+        restaurant['regional_label'] = regional_label
 
+    headers = []
+    for i, rest in enumerate(data):
+        if i == 0:
+            for head in rest:
+                headers.append(head)
+
+    with open('res_label.csv', 'w', newline = "") as csv_file:  
+        writer = csv.DictWriter(csv_file, fieldnames=headers)
+        writer.writeheader()
+        for row in data:
+            writer.writerow(row)
 
