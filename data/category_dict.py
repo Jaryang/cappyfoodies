bff7782893c93a7ae47a375868085357e38b04b3
Miao Li <mli628@linux6.cs.uchicago.edu>
2023-03-03 21:32:35 -0600

    file

---
+++ data/category_dict.py
@@ -0,0 +1,20 @@
+cate_dct = {
+    'Bakeries' : ["Bakeries", "Piadina", "Pretzels", "Bagels", 'Patisserie/Cake Shop'],
+    'Alcohol' : ['Cocktail Bars','Brewpubs','Beer Bar','Whiskey Bars','Wine Bars','Beer, Wine & Spirits', 'Pubs', 'Bars','Dive Bars', \
+                 'Sports Bars','Beer', 'Wine & Spirits', 'Breweries', 'Cideries', 'Distilleries', 'Meaderies', 'Wineries', 'Wine Tasting Room'],
+    'Coffee_Tea' : ['Coffee & Tea','Coffee Roasteries', 'Tea Rooms,Bubble Tea', 'Cafes'],
+    'Dessert' : ['Creperies','Candy Stores','Pancakes','Desserts','Chimney Cakes','Cupcakes','Custom Cakes','Donuts', 'Gelato', 'Honey', \
+                 'Ice Cream & Frozen Yogurt', 'Milkshake Bars', 'Shaved Ice', 'Shaved Snow', 'Candy Stores', 'Chocolatiers & Shops', 'Macarons'],
+    'Grocery' : ['Imported Food', 'International Grocery','Organic Stores','Fruits & Veggies','Health Markets','Convenience Stores','Farmers Market','CSA'],
+    'Beverage' : ['Juice Bars & Smoothies', 'Acai Bowls','Kombucha', 'Water Stores', 'Bubble Tea'],
+    'Fast_Food': ['Cheesesteaks','Burgers', 'Chicken Wings','Fast Food','Pizza', 'Hot Dogs', 'Sandwiches'],
+    'Specialty_Store' : ['Cheese Shops','Herbs & Spices','Olive Oil','Pasta Shops','Popcorn Shops','Tofu Shops','Empanadas'],
+    'Meat_Shop' : ['Seafood Markets', 'Butcher', 'Smokehouse'],
+    'Regional' : ['Cuban', 'Venezuelan','Honduran','Vietnamese','Afghan','African','Bulgarian','Colombian', 'Japanese', 'Laotian', 'Latin American', \
+                  'Mediterranean', 'Mexican', 'Middle Eastern', 'Modern European', 'New Mexican Cuisine', 'Pakistani','Pan Asian', 'Polish', 'Puerto Rican', 'Scottish', \
+                  'Southern','Thai', 'Turkish','Chinese', 'Czech','Filipino','French','Greek', 'Hawaiian', 'Indian', 'Irish', 'Korean','German','Italian',\
+                  'Japanese','Czech','American (Traditional)','American (New)','Asian Fusion', 'Cajun/Creole','Cantonese','Caribbean']
+    }
+Not_Food = ['Kids Activities', 'Music Venues', 'Arcades','Venues & Event Spaces','Party & Event Planning','Comedy Clubs', \
+            'Street Vendors','Bookstores','Lounges', 'Vape Shops','Gift Shops']
+Sub_Category = ['Halal','Noodles','Tacos', 'Sushi Bars','Soul Food', 'Dim Sum', 'Buffets', 'Cafeteria', 'Food Trucks','Gluten-Free','Caterers','Vegan','Diners']
