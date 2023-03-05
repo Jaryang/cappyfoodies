3c3509add726d1b5e777ab616cbbb7b2e6d1a87e
Maxine Ling Xu <maxinexu@linux5.cs.uchicago.edu>
2023-03-04 19:39:23 -0600

    Numbers to strings and fixing import statements

--- cappyfoodies/app.py
+++ cappyfoodies/app.py
@@ -1,10 +1,10 @@
 import sys
 import warnings
 warnings.filterwarnings("ignore")
-from .dashboard import dashboard
-from .scraping_data.pantry_scraper import food_pantry_tbl, lat_long
-from .scraping_data.yelp_simulation import yelp_simul
-from .scraping_data.yelp_api import get_businesses, get_reviews
+# from .dashboard import dashboard
+from scraping_data.pantry_scraper import food_pantry_tbl, lat_long
+from scraping_data.yelp_simulation import yelp_simul
+from scraping_data.yelp_api import get_businesses, get_reviews
 
 
 def run_dashboard():
@@ -52,27 +52,27 @@ def run():
                 (3) Download new data
                 (4) Quit program.
                 Option: """)
-    if user_input == 1:
+    if user_input == "1":
         print("running dashboard...")
         run_dashboard()
-    elif user_input == 2:
+    elif user_input == "2":
         print("running data cleaning...")
         run_clean()
-    elif user_input == 3:
+    elif user_input == "3":
         getdata_user_input = input(
             """Please type 
                 (1) Scrape the list of emergency pantries from Cook County's Sheriff's Office,
                 (2) Simulate interacting with Yelp's API,
-                (3) gather the full dataset of reviews for restaurants
+                (3) Gather the full dataset of reviews for restaurants
                 in Cook County using Yelp's API, or
                 (4) or anything else for quit program.""")
-        if getdata_user_input == 1:
+        if getdata_user_input == "1":
             print("Scraping data...")
             run_pantry_scraper()
-        elif getdata_user_input == 2:
+        elif getdata_user_input == "2":
             print("Starting Simulation...")
             run_api_simulation()
-        elif getdata_user_input == 3:
+        elif getdata_user_input == "3":
             print("Getting reviews")
             run_yelp_reviews()
         else:
