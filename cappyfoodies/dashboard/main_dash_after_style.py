f03f84e9f79508166d5bc397f0ebd833a5e42cc1
Jaryang <111720298+Jaryang@users.noreply.github.com>
2023-03-06 08:42:20 -0600

    Update main_dash_after_style.py

--- cappyfoodies/dashboard/main_dash_after_style.py
+++ cappyfoodies/dashboard/main_dash_after_style.py
@@ -414,7 +414,7 @@ def update_figure(selected_zipcode):
     
     fig = px.bar(row, x = "Race", y = 'Percentage')
     
-    fig.update_layout(transition_duration=500)
+    fig.update_layout(transition_duration=500, plot_bgcolor='#FDF5E6')
 
     return fig
 
